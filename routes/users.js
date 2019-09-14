1a7eb4cf93bf5318de2ce94146e9542c62e57506
ork821 <ork821@mail.ru>
2019-09-14 20:49:06 +0300

    Adding user to db

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const {check, validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs')
 
 
 router.post('/', [
@@ -13,8 +16,49 @@ router.post('/', [
         .isLength({
             min: 6
         })
-], (req, res) => {
-    res.send(req.body);
+], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    const {name, email, password} = req.body;
+    try {
+        let user = await User.findOne({email})
+        if (user) {
+            return res.status(400).json({msg: 'User already exists'})
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if (err) {
+                throw err;
+            }
+            res.json({token})
+        })
+
+    } catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server Error')
+
+    }
 });
 
 module.exports = router;
