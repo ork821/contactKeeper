38c71190a541d84d18953950b7ffc3067f1d52c4
ork821 <ork821@mail.ru>
2019-09-16 10:08:49 +0300

    Added Router component and navigation

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Navbar = ({title = "Contact Keeper", icon = 'fas fa-id-card-alt'}) => {
     return (
@@ -6,6 +7,10 @@ const Navbar = ({title = "Contact Keeper", icon = 'fas fa-id-card-alt'}) => {
             <h1>
                 <i className={icon}/>{title}
             </h1>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
         </div>
     );
 };
